9ca1ee3fb819cd977e515dae7e2b177176136335
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-12-15 17:24:45 +0600

    Statistics part done some due

--- src/Componants/AdminPanel/DashBoardTop/DashBoardTop.js
+++ src/Componants/AdminPanel/DashBoardTop/DashBoardTop.js
@@ -10,7 +10,7 @@ const DashBoardTop = () => {
                 <div className="col-md-12 dashBoardMainDiv px-0">
                     <div className="dashBoardBar bg-secondary align-items-center d-flex justify-content-end">
                         <img className="img-fluid flag mr-5" src={flag} alt="flag"/>
-                        <h4 className="text-light mr-5"><i class="fas fa-bell"></i></h4>
+                        <h4 className="text-light mr-5"><i class="fas fa-bell text-light"></i></h4>
                         <h4 className="text-warning">Hi, <span className="text-light mr-3">Abir</span> </h4>
                         <img className="img-fluid avater mr-3" src={people} alt="avater"/>
                         <h4 className="text-light"><i class="fas fa-cog mr-3 px-3"></i></h4>
--- src/Componants/Statistics/Statistics.js
+++ src/Componants/Statistics/Statistics.js
@@ -1,16 +1,66 @@
 import React from 'react';
-import AdminPanel from '../AdminPanel/AdminPanel';
-
+import './Statistics.css';
+import welcomeProfileImg from "../../images/dashboard/profile-img.png";
+import proPic from "../../images/dashboard/abir.JPG";
 const Statistics = () => {
     return (
         <div className="container">
-            <div className="row">
+            <div className="row dashBoardHeight">
                 {/* <div className="col-md-3 dashBoardMenu bg-dark px-0">
                     <AdminPanel></AdminPanel>
                 </div> */}
-                <div className="col-md-9 dashBoardMainDiv px-0">
+                <div className="col-md-12 dashBoardMainDiv ">
                     <div className="">
-                        <h1>Statistics</h1>
+                        <h5 className="text-primary py-2">DASHBOARD</h5>
+                        <div className="welcome-back">
+                            <div className="row ">
+                                <div className="col-md-5 mt-2 dasBody pr-0 pl-md-2 pl-0">
+                                    <div className="row ml-0 mr-0 bg-dark">
+                                        <div className="col-md-7 text-light pb-3 pt-3">
+                                            <h5>Welcome Back !</h5>
+                                            <h6>Abir Hasan</h6>
+                                        </div>
+                                        <div className="col-md-5">
+                                            <img className="img-fluid" src={welcomeProfileImg} alt="welcome" />
+                                        </div>
+                                    </div>
+                                    <div className="welcomeProfile bg-light">
+                                        <div className="row p-2">
+                                            <div className="col-md-6">
+                                                <img className="img-fluid proPicofDshB" src={proPic} alt="profile pic" />
+                                                <h6 className="pt-2">Abir Hasan</h6>
+                                                <p>Full-Stack Devoloper</p>
+                                            </div>
+                                            <div className="col-md-3 p-3">
+                                                <h5>125</h5>
+                                                <small><strong>Projects</strong></small>
+                                            </div>
+                                            <div className="col-md-3 p-3">
+                                                <h5>$1025</h5>
+                                                <small><strong>Revenue</strong></small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3 ml-md-3 mb-2 mt-2 d-flex orderCount justify-content-between align-items-center">
+                                    <div>
+                                        <p className="m-0"><strong>Orders</strong></p>
+                                        <h5>1,225</h5>
+                                    </div>
+                                    <i class="far fa-copy bg-primary text-white" style={{ lineHeight: "40px" }}></i>
+                                </div>
+
+                                <div className="col-md-3 mt-2 d-flex orderCount justify-content-between align-items-center ml-md-4">
+                                    <div>
+                                        <p className="m-0"><strong>Revenue</strong></p>
+                                        <h5>$35, 723</h5>
+                                    </div>
+                                    <i class="far fa-copy bg-primary text-white" style={{ lineHeight: "40px" }}></i>
+                                </div>
+                                
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
