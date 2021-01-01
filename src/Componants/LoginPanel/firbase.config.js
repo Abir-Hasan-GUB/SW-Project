7273def6b654f3c650274565b7161ec76ecb9af9
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2021-01-01 12:03:27 +0600

    Untrack login auth key

--- src/Componants/LoginPanel/firbase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAPJTDMhbc1Uj5tjClXcNBh7lDQB4osIyI",
-    authDomain: "se-final-project-12a91.firebaseapp.com",
-    projectId: "se-final-project-12a91",
-    storageBucket: "se-final-project-12a91.appspot.com",
-    messagingSenderId: "1000701586808",
-    appId: "1:1000701586808:web:67097713eae49427aa2c8d"
-  };
-  export default firebaseConfig;
